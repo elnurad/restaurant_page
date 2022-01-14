da8af1f5e18354def886c26093f232ede349c617
Elnura D <elnurab@gmail.com>
2022-01-14 12:18:14 +0800

    styles.css

--- src/about.js
+++ src/about.js
@@ -5,16 +5,19 @@ function aboutPage() {
     const about = document.createElement('section');
     about.classList.add("home") //<div class="about"></div>
 
-    const steakImage = document.createElement("img");
-    steakImage.src = "images/lavache.jpeg"; 
-    steakImage.alt = "image of a nice steak meal on a restaurant table";
-    steakImage.classList.add('aboutImage');
+    const frontImage = document.createElement("img");
+    frontImage.src = "images/lavacheFront.jpeg"; 
+    frontImage.alt = "front of the restaurant at night";
+    frontImage.classList.add('aboutImage');
+    frontImage.id = "frontImage";
+    
+
     const cowImage = document.createElement('img')
     cowImage.src = 'images/cow.png'
     cowImage.alt = "image of a cow drawing the Eifel Tower"
     cowImage.classList.add("aboutImage")
-    
-    about.appendChild(steakImage);
+   
+    about.appendChild(frontImage);
     const text = "La Vache! pays homage to the tradition of Parisian entrecôte steakhouses. With locations in Hong Kong’s SoHo, Tsim Sha Tsui and Admiralty neighbourhoods, the lively brasserie elevates the simple steak frites menu to a new level: an organic green salad followed by richly marbled USDA Prime ribeye and crispy frites served with generous pots of La Vache!‘s secret sauce. Premium ingredients, upbeat music and friendly service have established La Vache!’s steak frites as a favourite among Hong Kong’s dining scene."
     
     about.appendChild(createParagraph(text));
@@ -22,14 +25,6 @@ function aboutPage() {
     return about
 }
 
-
-
-// function createParagraph(text) {
-//     const p = document.createElement('p');
-//     p.textContent = text;
-//     return p
-
-// }
 
 function createAbout(e) {
     e.preventDefault()
--- src/menu.js
+++ src/menu.js
@@ -2,11 +2,13 @@ import createParagraph from './createParagraph';
 import createH3 from './createH3';
 
 function menuPage() {
+    const menuPageWrapper = document.createElement('div')
+    menuPageWrapper.classList.add('menuPageWrapper');
     const menuContainer = document.createElement('div'); //div to hold two parts of menu
     menuContainer.classList.add("menuContainer")
-    const englishMenu = document.createElement('div')
+    const englishMenu = document.createElement('div') //container for english menu
     const frenchMenu = document.createElement('div')
-    frenchMenu.classList.add("frenchMenu")
+    frenchMenu.classList.add("frenchMenu") //container for french menu
     englishMenu.classList.add("englishMenu")
 
     englishMenu.appendChild(createH3("Today"))
@@ -14,19 +16,35 @@ function menuPage() {
     const englishPrice = "$398 per person"
     englishMenu.appendChild(createParagraph(englishText))
     englishMenu.appendChild(createParagraph(englishPrice))
+
+    const frenchMenuDiv = document.createElement('div');
+    frenchMenuDiv.classList.add('frenchMenuDiv')
+    const jumpingCowImg = document.createElement('img');
+    jumpingCowImg.src = 'images/jumping-cow.png';
+    jumpingCowImg.id = "jumpingCowImg"
+    frenchMenuDiv.appendChild(jumpingCowImg)
     
     const frenchText = "Entrecôte “La Vache!” avec sauce maison, accompagnée de frites et salade verte aux noix"
     const frenchPrice = "$398 par personne"
     frenchMenu.appendChild(createH3("Aujourd’hui"))
     frenchMenu.appendChild(createParagraph(frenchText))
     frenchMenu.appendChild(createParagraph(frenchPrice))
+    frenchMenuDiv.appendChild(frenchMenu)
+
+    const beverageMenu = document.createElement('a');
+    beverageMenu.classList.add('beverageMenu')
+    beverageMenu.textContent = "See Our Beverage Menu Here"
+    beverageMenu.href = "images/beverageMenu.jpeg"
+    beverageMenu.target = "_blank"
     
     
     menuContainer.appendChild(englishMenu)
-    menuContainer.appendChild(frenchMenu)
+    menuContainer.appendChild(frenchMenuDiv)
+    menuPageWrapper.appendChild(menuContainer)
+    menuPageWrapper.appendChild(beverageMenu)
 
 
-    return menuContainer
+    return menuPageWrapper
 
 
 }
